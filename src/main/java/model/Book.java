b97693b86def8300736d2d8fa2756c53c7cf3e6a
DaXiPL <dawid.wypych04@gmail.com>
2021-03-29 10:51:57 +0200

    push

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -7,13 +7,15 @@ public class Book {
     private String author;
     private String publisher;
     private int pages;
+    private int id;
     private LocalDate addDate;
 
-    public Book(String title, String author, String publisher, int pages, LocalDate addDate) {
+    public Book(String title, String author, String publisher, int pages, int id, LocalDate addDate) {
         this.title = title;
         this.author = author;
         this.publisher = publisher;
         this.pages = pages;
+        this.id = id;
         this.addDate = addDate;
     }
 }
