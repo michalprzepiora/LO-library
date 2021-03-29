66bb3581c2376a47cf4536e38d0b67b44c0155f0
DaXiPL <dawid.wypych04@gmail.com>
2021-03-29 20:51:43 +0200

    book repository

---
+++ src/main/java/repository/BookRepository.java
@@ -0,0 +1,30 @@
+package repository;
+
+import model.Book;
+
+public class BookRepository implements BookInterface{
+    @Override
+    public Book addBook(Book book) {
+        return null;
+    }
+
+    @Override
+    public Book removeBook(Book book) {
+        return null;
+    }
+
+    @Override
+    public Book getAll() {
+        return null;
+    }
+
+    @Override
+    public Book getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Book getByName(String name) {
+        return null;
+    }
+}
