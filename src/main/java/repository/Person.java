d1561d20b12e9ef771885e88f72b795f7fb7bd78
Paweł Kupis <pawel.kupis@o2.pl>
2021-03-30 17:06:31 +0200

    generator +repozitory dadfasdsa

---
+++ src/main/java/repository/Person.java
@@ -0,0 +1,44 @@
+
+
+package repository;
+
+
+
+public class Person implements PersonInterface {
+    int id;
+    String name;
+    String surname;
+    String eMail;
+    String number;
+    public Person(String name, String surname, String eMail, String number) {
+        this.name = name;
+        this.surname = surname;
+        this.eMail = eMail;
+        this.number = number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void add() {
+
+    }
+
+}
