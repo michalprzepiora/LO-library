32cb8f3574d3ee4eab1875e8f01352fe05cdd05c
DaXiPL <dawid.wypych04@gmail.com>
2021-03-30 17:18:47 +0200

    update

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -1,21 +1,44 @@
 package model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 
 public class Book {
     private String title;
     private String author;
-    private String publisher;
-    private int pages;
     private int id;
+    private int pages;
     private LocalDate addDate;
 
+    public Book(String title, String author, int pages, int id, LocalDate addDate) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.id = id;
+        this.addDate = addDate;
+    }
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getAddDate() {
+        return addDate;
+    }
 }
