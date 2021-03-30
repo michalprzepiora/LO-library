0f27f07681ded96d1bdf2213a5d43898515be910
DaXiPL <dawid.wypych04@gmail.com>
2021-03-30 17:29:32 +0200

    update

--- src/main/java/repository/BookRepository.java
+++ src/main/java/repository/BookRepository.java
@@ -2,29 +2,10 @@ package repository;
 
 import model.Book;
 
-public class BookRepository implements BookInterface{
-    @Override
-    public Book addBook(Book book) {
-        return null;
-    }
-
-    @Override
-    public Book removeBook(Book book) {
-        return null;
-    }
-
-    @Override
-    public Book getAll() {
-        return null;
-    }
-
-    @Override
-    public Book getById(int id) {
-        return null;
-    }
-
-    @Override
-    public Book getByName(String name) {
-        return null;
-    }
+public interface BookRepository {
+    Book addBook(Book book);
+    Book removeBook(Book book);
+    Book getAll();
+    Book getById(int id);
+    Book getByName(String name);
 }
