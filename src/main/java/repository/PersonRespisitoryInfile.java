27357a9befea2a07c2d44c2d00abd2c861ea6903
Paweł Kupis <pawel.kupis@o2.pl>
2021-04-01 18:03:49 +0200

    niedokońvczone repo jeszcze

---
+++ src/main/java/repository/PersonRespisitoryInfile.java
@@ -0,0 +1,17 @@
+package repository;
+
+import model.Person;
+
+import java.util.List;
+
+public class PersonRespisitoryInfile implements PersonRepository {
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public List<Person> getAll() {
+        return null;
+    }
+}
